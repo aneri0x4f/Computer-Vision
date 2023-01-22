2c36a3886ee12bf96bc984b55f2b55854f35b3d1
Aneri Dalwadi <57945364+aneri0x4f@users.noreply.github.com>
2023-01-22 19:41:12 +0530

    Update VertexCoordFromContour.py

--- VertexCoordFromContour.py
+++ VertexCoordFromContour.py
@@ -1,11 +1,9 @@
 """
 Get co-ordinates of vertices from the contour
-
 Requirements:
 - python
 - basic knowledge on usage of comand line
 - NumPy 
-
 Approach:
 -The co-ordinates of each vertices of a contour is hidden in the contour itself. 
 - We will be using numpy library to convert all the co-ordinates of a contour 
@@ -14,7 +12,6 @@ of each contour.
 - The key point here is that the first co-ordinate in the array would always 
 be the co-ordinate of the topmost vertex and hence could help in detection of 
 orientation of an image.
-
 Recomendations:
 - prefer an image with dark background
 - for other cases you can handle the threshold or use inRange instead
@@ -76,7 +73,7 @@ def getCoordinates(image, cs):
                     # text on remaining co-ordinates.
                     cv2.putText(image, coord, (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 255, 0)) 
 
-        return vertices            
+    return vertices            
 
 print(getCoordinates(image, cs))           
 cv2.imshow("image", image)
